ad15ebf3aed70f0fe46d613a4f0b1e1a734b781d
Peshekhonov Maksim <peshekhonov.m@citilink.ru>
2021-05-22 16:11:21 +0300

    группа прожекторов, новые инструменты, правки

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import {Canvas} from '@react-three/fiber';
+import {Sky, PointerLockControls} from '@react-three/drei';
 import Box from './Box';
 
 const MainPage = () => {
   return (
     <Canvas>
+      <Sky sunPosition={[100, 10, 100]} />
       <pointLight position={[10, 10, 10]} />
       <Box position={[-1.2, 0, 0]} />
       <Box position={[1.2, 0, 0]} />
+      <PointerLockControls />
+      <group name="projectors">
+        <mesh position={[-45, 0, 0]} castShadow={true}>
+          <boxBufferGeometry args={[2, 1, 2]} />
+          <meshLambertMaterial color="#69f" />
+        </mesh>
+      </group>
     </Canvas>
   );
 };
