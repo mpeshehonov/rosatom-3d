95e4c94ba3a0d5b3d9406ed259d01e6fbd069935
Peshekhonov Maksim <peshekhonov.m@citilink.ru>
2021-05-22 11:13:35 +0300

    working example

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,18 @@
-import React, {createContext, useState} from 'react';
+import React from 'react';
 import PageLayout from './shared/components/PageLayout';
-import Routes from './pages/routes';
-import {
-  BrowserRouter as Router,
-  Redirect,
-  Route,
-  Switch,
-} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import 'antd/dist/antd.less';
 import './styles.scss';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
-import ForgotPage from './pages/ForgotPage';
-
-export const UserContext = createContext<any | null>(null);
+import MainPage from './pages/MainPage';
 
 const App = () => {
-  const [user, setUser] = useState<any | null>(null);
-
-  const changeUser = (newUser: object) => setUser(newUser);
-
   return (
     <Router>
-      <UserContext.Provider value={[user, changeUser]}>
-        <PageLayout>
-          <Switch>
-            <Route path="/main" exact />
-            <Route path="/" exact>
-              <Redirect to="/login" />
-            </Route>
-            <Route path="/login" component={LoginPage} exact />
-            <Route path="/register" component={RegisterPage} exact />
-            <Route path="/forgot" component={ForgotPage} exact />
-          </Switch>
-        </PageLayout>
-      </UserContext.Provider>
+      <PageLayout>
+        <Switch>
+          <Route path="/" component={MainPage} exact />
+        </Switch>
+      </PageLayout>
     </Router>
   );
 };
--- src/shared/components/Header/Header.tsx
+++ src/shared/components/Header/Header.tsx
@@ -1,53 +1,21 @@
-import React, {useContext} from 'react';
-import {Layout, Button, Typography} from 'antd';
+import React from 'react';
+import {Layout, Button} from 'antd';
 import './styles.scss';
-import {Link, useLocation, useHistory} from 'react-router-dom';
-import {UserContext} from '../../../App';
-
-const {Text} = Typography;
+import {Link} from 'react-router-dom';
 
 const Header = () => {
-  const location = useLocation();
-  const history = useHistory();
-  const [user, changeUser] = useContext(UserContext);
-
-  const logout = () => {
-    changeUser(null);
-    history.push('/');
-  };
-
   return (
     <Layout.Header className="header">
       <Link to="/" className="header__logo">
         РОСАТОМ
       </Link>
-      {user ? (
-        <Button type="link" onClick={logout}>
-          Выйти
-        </Button>
-      ) : (
-        <>
-          {location.pathname === '/register' ? (
-            <div className="header__button">
-              <Text className="mr-1">Уже есть аккаунт?</Text>
-              <Link to="/login">
-                <Button type="primary" shape="round" size="large">
-                  Войти
-                </Button>
-              </Link>
-            </div>
-          ) : (
-            <div className="header__button">
-              <Text className="mr-1">У вас нет аккаунта?</Text>
-              <Link to="/register">
-                <Button type="primary" shape="round" size="large">
-                  Регистрация
-                </Button>
-              </Link>
-            </div>
-          )}
-        </>
-      )}
+      <div className="header__button">
+        <Link to="/panel">
+          <Button type="primary" shape="round" size="large">
+            Панель
+          </Button>
+        </Link>
+      </div>
     </Layout.Header>
   );
 };
