867820e4f6b06842e19b2ecab2e3c83c82818cfc
3l <djelement@yandex.ru>
2021-05-22 13:35:31 +0300

    #el added sign object

---
+++ src/pages/ElPage/component/PolyObject.jsx
@@ -0,0 +1,26 @@
+const PolyObject = () => {
+    return (
+        <>
+            <svg>
+                <polygon data-id="1" points="388,117 393,117 431,108 431,178 388,178"></polygon>
+                <polygon data-id="2" points="433,108 477,98 477,178 433,178"></polygon>
+                <polygon data-id="3" points="479,97 520,89 520,178 479,178"></polygon>
+                <polygon data-id="4" points="521,88 560,79 560,178 521,178"></polygon>
+                <polygon data-id="5" points="562,79 622,66 662,178 562,178"></polygon>
+                <polygon data-id="6" points="388,206 431,206 431,262 388,262"></polygon>
+                <polygon data-id="7" points="433,206 477,206 477,262 433,262"></polygon>
+                <polygon data-id="8" points="479,206 520,206 520,262 479,262"></polygon>
+                <polygon data-id="9" points="521,206 562,206 562,262 521,262"></polygon>
+                <polygon data-id="10" points="564,206 604,206 604,262 564,262"></polygon>
+                <polygon data-id="11" points="393,292 431,292 431,364 393,364"></polygon>
+                <polygon data-id="12" points="433,292 477,292 477,364 433,364"></polygon>
+                <polygon data-id="13" points="479,292 520,292 520,364 479,364"></polygon>
+                <polygon data-id="14" points="521,292 562,292 562,364 521,364"></polygon>
+                <polygon data-id="15" points="564,292 604,292 604,364 564,364"></polygon>
+                <polygon data-id="16" points="606,292 647,292 647,364 606,364"></polygon>
+            </svg>
+        </>
+    )
+}
+
+export default PolyObject
---
+++ src/pages/ElPage/component/SignatureObject.jsx
@@ -0,0 +1,27 @@
+const SignatureObject = () => {
+    return (
+        <>
+            <div className="scheme-item" data-id="1" style={{"top": "137px", "left": "408px"}}>
+                <div className="scheme-name">Склад_<br/>1</div>
+                <div className="scheme-popup">Горючие материалы</div>
+            </div>
+            <div className="scheme-item" data-id="2" style={{"top": "137px", "left": "453px"}}>
+                <div className="scheme-name">Склад_<br/>2</div>
+                <div className="scheme-popup">Горючие материалы</div>
+            </div>
+            <div className="scheme-item" data-id="3" style={{"top": "137px", "left": "498px"}}>
+                <div className="scheme-name">Склад_<br/>3</div>
+                <div className="scheme-popup">Металлолом</div>
+            </div>
+            <div className="scheme-item" data-id="4" style={{"top": "137px", "left": "539px"}}>
+                <div className="scheme-name">Склад_<br/>4</div>
+                <div className="scheme-popup">Химикаты</div>
+            </div>
+            <div className="scheme-item" data-id="5" style={{"top": "137px", "left": "598px"}}>
+                <div className="scheme-name">Склад_<br/>5</div>
+                <div className="scheme-popup">Химикаты</div>
+            </div>
+        </>
+    )
+}
+export default SignatureObject
