ae6d6a71767ba6ae0bb07cdb1218cbd4f2218f32
Peshekhonov Maksim <peshekhonov.m@citilink.ru>
2021-05-22 22:54:35 +0300

    доработки по земле

--- src/pages/MainPage/Ground/Ground.tsx
+++ src/pages/MainPage/Ground/Ground.tsx
@@ -4,9 +4,14 @@ import {PlaneProps, usePlane} from '@react-three/cannon';
 export const Ground = (props: PlaneProps) => {
   const [ref] = usePlane(() => ({rotation: [-Math.PI / 2, 0, 0], ...props}));
   return (
-    <mesh ref={ref} receiveShadow>
-      <planeGeometry args={[1000, 1000]} />
-      <meshStandardMaterial />
+    <mesh
+      ref={ref}
+      receiveShadow={true}
+      rotation={[Math.PI / 2, 0, 0]}
+      position={[0, -0.65, 0]}
+    >
+      <planeGeometry args={[2000, 2000]} />
+      <meshPhongMaterial color={0x948d7d} depthWrite={false} />
     </mesh>
   );
 };
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -31,52 +31,52 @@ const MainPage = () => {
         <Ground />
         <Box position={[-1.2, 1, 0]} />
         <Box position={[1.2, 1, 0]} />
+        <Suspense fallback={null}>
+          <Buildings />
+        </Suspense>
+        <group name="projectors">
+          <mesh position={[-45, 0, 0]} castShadow={true}>
+            <boxBufferGeometry args={[2, 1, 2]} />
+            <meshLambertMaterial color="#69f" />
+          </mesh>
+          <mesh position={[-50, 0, 0]} castShadow={true}>
+            <boxBufferGeometry args={[2, 1, 2]} />
+            <meshLambertMaterial color="#69f" />
+          </mesh>
+          <mesh position={[0, 2, 0]} rotation={[0, 2, 0]} castShadow={true}>
+            <cylinderGeometry args={[0.2, 0.2, 0.4, 20]} />
+            <meshPhongMaterial color="#330099" />
+            <spotLight
+              args={[0xffffff, 0.5, 150]}
+              position={[0, 2, 0]}
+              rotation={[0, 2, 0]}
+              castShadow={true}
+              power={1.5}
+              angle={0.5}
+              decay={2}
+              penumbra={0.1}
+              distance={200}
+              shadowCameraNear={0.01}
+            />
+          </mesh>
+          <mesh position={[2, 2, 0]} rotation={[0, 2, 0]} castShadow={true}>
+            <cylinderGeometry args={[0.2, 0.2, 0.4, 20]} />
+            <meshPhongMaterial color="#330099" />
+            <spotLight
+              args={[0xffffff, 0.5, 150]}
+              position={[2, 2, 0]}
+              rotation={[0, 2, 0]}
+              castShadow={true}
+              power={1.5}
+              angle={0.5}
+              decay={2}
+              penumbra={0.1}
+              distance={200}
+              shadowCameraNear={0.01}
+            />
+          </mesh>
+        </group>
       </Physics>
-      <Suspense fallback={null}>
-        <Buildings />
-      </Suspense>
-      <group name="projectors">
-        <mesh position={[-45, 0, 0]} castShadow={true}>
-          <boxBufferGeometry args={[2, 1, 2]} />
-          <meshLambertMaterial color="#69f" />
-        </mesh>
-        <mesh position={[-50, 0, 0]} castShadow={true}>
-          <boxBufferGeometry args={[2, 1, 2]} />
-          <meshLambertMaterial color="#69f" />
-        </mesh>
-        <mesh position={[0, 2, 0]} rotation={[0, 2, 0]} castShadow={true}>
-          <cylinderGeometry args={[0.2, 0.2, 0.4, 20]} />
-          <meshPhongMaterial color="#330099" />
-          <spotLight
-            args={[0xffffff, 0.5, 150]}
-            position={[0, 2, 0]}
-            rotation={[0, 2, 0]}
-            castShadow={true}
-            power={1.5}
-            angle={0.5}
-            decay={2}
-            penumbra={0.1}
-            distance={200}
-            shadowCameraNear={0.01}
-          />
-        </mesh>
-        <mesh position={[2, 2, 0]} rotation={[0, 2, 0]} castShadow={true}>
-          <cylinderGeometry args={[0.2, 0.2, 0.4, 20]} />
-          <meshPhongMaterial color="#330099" />
-          <spotLight
-            args={[0xffffff, 0.5, 150]}
-            position={[0, 2, 0]}
-            rotation={[0, 2, 0]}
-            castShadow={true}
-            power={1.5}
-            angle={0.5}
-            decay={2}
-            penumbra={0.1}
-            distance={200}
-            shadowCameraNear={0.01}
-          />
-        </mesh>
-      </group>
     </Canvas>
   );
 };
